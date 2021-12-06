1f49b9500fde40fe637dbf0af0d4aa13ff1a9ee5
akinhet <piti200406@gmail.com>
2021-12-06 10:19:33 +0100

    Symlink test

--- projects/test-integration/main/display.h
+++ projects/test-integration/main/display.h
@@ -1,12 +1,1 @@
-#pragma once
-
-//#include <stdint.h>
-
-#define LCD_WIDTH 320
-#define LCD_HEIGHT 240
-#define LCD_DEPTH 2
-
-//static uint16_t frameBuffer[LCD_WIDTH * LCD_HEIGHT]; //Example of frame buffer declaration
-
-void setupDisplay(void);
-void frameDraw(uint16_t* buffer);
+../../../src/display.h
