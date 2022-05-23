84e3448073d8378eda8e1a53982a9eec73e44adc
Pietrzak04 <piotr.mijakowski04@gmail.com>
2022-05-23 23:02:37 +0200

    input cleanup

--- src/input.h
+++ src/input.h
@@ -1,26 +1,12 @@
 #pragma once
+#include <string.h>
+#include <esp_log.h>
+#include <driver/i2c.h>
 
-#define INPUT_CLK_SPEED 800000
+#define INPUT_CLK_SPEED 1000000
 
 #define INPUT_I2C_ADDR_0	0x27
 #define INPUT_I2C_ADDR_1	0x26
-
-#define INPUT_I2C_PORT		0
-
-#define INPUT_REG_IODIR		0x00
-#define INPUT_REG_IPOL		0x01
-#define INPUT_REG_IOCON		0x05
-#define INPUT_REG_GPPU		0x06
-#define INPUT_REG_GPIO		0x09
-
-#define TIME_OUT 1000
-#define TIME_DELAY 1000
-
-#define ACK_CHECK_EN 0x1 
-#define ACK_VAL    0x0
-#define NACK_VAL   0x1
-
-#define TAG "Input"
 
 //#define INPUT_LOGGING //uncomment to recive informational logs in console
 
@@ -42,5 +28,3 @@ typedef struct{
 
 void setupInput(void);
 Input pollInput(void);
-esp_err_t mcp23008_write_reg(uint8_t ICAddress, uint8_t regAddress, uint8_t value);
-esp_err_t mcp23008_read_reg(uint8_t ICAddress, uint8_t regAddress, uint8_t* value);
