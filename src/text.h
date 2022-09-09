1b2fac3a5097e4446e7fd48130b3be0e31885489
akinhet <piti200406@gmail.com>
2022-09-09 12:35:37 +0200

    Added sprites and menu to snake

--- src/text.h
+++ src/text.h
@@ -5,6 +5,4 @@
 
 const int defaultFont[96][48];
 
-/* void defaultDrawText(int x, int y, const char *str, uint16_t *buffer); */
-/* void drawText(Tileset font, int x, int y, const char *str, uint16_t *buffer); */
 void drawText(int x, int y, int scale, uint16_t color, const char *str, uint16_t *buffer);
